7fb3cbb5cd6c4f60336703aab4bcf6cefba491c3
OO39XP <stanley.tsui@ing.com>
2023-08-14 11:16:44 +0200

    Added some personal notes regarding JPA queries

--- src/main/java/com/example/monsterhunter/repository/EquipmentListRepository.java
+++ src/main/java/com/example/monsterhunter/repository/EquipmentListRepository.java
@@ -3,11 +3,15 @@ package com.example.monsterhunter.repository;
 import com.example.monsterhunter.domain.EquipmentList;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EquipmentListRepository extends JpaRepository<EquipmentList, String> {
     EquipmentList findByName(String name);
 
+    /*@Query("FROM EquipmentList WHERE name=:name") //SEARCH JQL
+    EquipmentList dflkajelfkajeof(String name); //Option 1*/
 
+    //EquipmentList findByNameAndWaterRes(String name); //Option 2: Query: Starts with "findBy" + parameters of the Entity + Connectors e.g. "And"
 }
